14c02f7220c57838f90dce7b7f8694180a879e71
Bruno Augusto <brunomorpheu@gmail.com>
2021-08-20 21:31:09 -0300

    create a function createSlide for creating a slide

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 const carousel = document.querySelector('.carousel');
 let sliders = [];
 
-let slideIndex = 0; 
+let slideIndex = 0;
+
+/* create function createSlide*/
+const createSlide = () => {
+    if(slideIndex >= movies.length){
+        slideIndex = 0;
+    }
+
+    let slide = document.createElement('div');
+    let imgElement = document.createElement('img');
+    let content = document.createElement('div');
+    let h1 = document.createElement('h1');
+    let p = document.createElement('p');
+}
